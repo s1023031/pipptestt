698d6d467e17f0328ca9de16884da7891ee68618
Yoong Siang Yun <doremon_yun@hotmail.com>
2018-11-12 14:15:09 +0800

    upload all

---
+++ pipptestt/__init__.py
@@ -0,0 +1,11 @@
+class ani():
+	def __init__(self):
+  		self.name = "TEST"
+
+def testpac():
+	return 123
+
+a = ani() 
+print(a.name)
+b = testpac()
+print(b)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='pipptestt',
+      version='0.1',
+      description='learning to use pip',
+      url='https://github.com/s1023031/pipptestt.git',
+      author='Aileene',
+      author_email='doremonyun0120@gmail.com',
+      license='MIT',
+      packages=['pipptestt','filter'],
+      zip_safe=False)
+--------------------- 
